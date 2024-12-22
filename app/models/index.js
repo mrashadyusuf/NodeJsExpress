78840f3f85b712da6ec0b29fc0d4a78c5590abd5
mrashadyusuf <mrashadyusuf@gmail.com>
2024-12-22 07:32:05 +0700

    crud user

--- app/models/index.js
+++ app/models/index.js
@@ -20,5 +20,6 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+db.users = require("./user.model.js")(sequelize, Sequelize);
 
 module.exports = db;
